1f25fa020d886f3fff0fc2235bd1daecb3831fd8
ViktorM <victor.makoviychuk@gmail.com>
2025-04-29 14:21:13 -0700

    Benchmarks.

--- cs336_systems/benchmark.py
+++ cs336_systems/benchmark.py
@@ -234,9 +234,9 @@ if __name__ == "__main__":
     model_sizes = {
         "small": {"d_model": 768, "d_ff": 3072, "num_layers": 12, "num_heads": 12},
         "medium": {"d_model": 1024, "d_ff": 4096, "num_layers": 24, "num_heads": 16},
-        "large": {"d_model": 1280, "d_ff": 5120, "num_layers": 36, "num_heads": 20},
-        "xl": {"d_model": 1600, "d_ff": 6400, "num_layers": 48, "num_heads": 25},
-        "2.7B": {"d_model": 2560, "d_ff": 10240, "num_layers": 32, "num_heads": 32},
+    #    "large": {"d_model": 1280, "d_ff": 5120, "num_layers": 36, "num_heads": 20},
+    #    "xl": {"d_model": 1600, "d_ff": 6400, "num_layers": 48, "num_heads": 25},
+    #    "2.7B": {"d_model": 2560, "d_ff": 10240, "num_layers": 32, "num_heads": 32},
     }
 
     output_markdown_file = "benchmark_results_" + "warmup_" + str(parsed_args.warmup_steps)
